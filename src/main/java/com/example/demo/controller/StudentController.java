d0209d78432d59206cfeee44342739a3a58fa058
bala-amypo <bala@amypo.in>
2025-12-18 09:41:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,20 +1,51 @@
-package com.example.demo.controller;
+package com.example.practice.controller;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Stuentity;
-import com.example.demo.service.StudentService;
+import com.example.practice.entity.StudentEntity;
+import com.example.practice.service.StudentService;
+
+
 
 @RestController
 public class StudentController {
-    @Autowired
-    StudentService studentService;
+         @Autowired
+         StudentService serv;
 
-    @PostMapping("/postdata")
-    public Stuentity postdata(@RequestBody Stuentity stuentity){
-        return studentService.saveStudent(stuentity);
-    }
+         @PostMapping("/create")
+         public StudentEntity createData(@RequestBody StudentEntity st){
+            return serv.insert(st);
+         }
+         
+         @GetMapping("/get")
+         public List<StudentEntity> getData(){
+            return serv.getAll();
+         }
+
+         @GetMapping("/get/{id}")
+         public Optional<StudentEntity> getbyId(@PathVariable Long id){
+            return serv.getId(id);
+
+         }
+
+         @PutMapping("/update/{id}")
+         public StudentEntity update(@PathVariable Long id,@RequestBody StudentEntity st){
+            return serv.updateById(id,st);
+           
+         }
+
+         @DeleteMapping("/delete")
+         public String delete(){
+            return serv.delete();
+         }
 }
